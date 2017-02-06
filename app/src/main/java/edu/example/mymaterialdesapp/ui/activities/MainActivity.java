d00edbc4f35c41fabd2ec4a1f4d481b0626ebc40
Alexandr Meleshko <meleshko_alexandr@mail.ru>
2017-02-06 10:29:15 +0200

    Initial commit

---
+++ app/src/main/java/edu/example/mymaterialdesapp/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.example.mymaterialdesapp.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.example.mymaterialdesapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
